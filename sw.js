faf56695959b524689144e6858187e801310b442
Vikram <Jimbobdegeh@yahoo.com>
2026-05-19 20:44:50 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,15 @@
 importScripts("https://cdn.jsdelivr.net/gh/AerialiteLabs/tinyjet-frontend@latest/tinyjet/scramjet.all.js");
 
-// Initialize the worker with your specific WISP and folder prefix
 const { ScramjetServiceWorker } = $scramjetLoadWorker();
 const scramjet = new ScramjetServiceWorker({
-    wisp: "wss://appointment-pose-mounts-republic.trycloudflare.com/wisp/",
+    wisp: "wss://gointospace.app/wisp/",
     prefix: "/frontended/scramjet/"
 });
 
 async function handleRequest(event) {
-  // If the request path matches our prefix, proxy it
   if (scramjet.route(event)) {
     return await scramjet.fetch(event);
   }
-  // Otherwise, let the browser load standard files normally
   return await fetch(event.request);
 }
 
